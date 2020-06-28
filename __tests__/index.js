bd85e81f19894a56b5a7229b6a0cb9b28a8a5abb
Tyler Cinnamon <tycinnamon@gmail.com>
2020-06-27 21:15:00 -0500

    test

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,12 +1,20 @@
 const babel = require('babel-core')
 const plugin = require('../')
 
-const example = `
+const examples = [`
   const Styles = {h1: {fontSize: 32}}
   const X = ({name}) => <Text className="h1">name</Text>
-`
+`,`
+  const Styles = {h1: {fontSize: 32}}
+  const X = ({name}) => <Text className="h1" style={{fontSize: 42}}>name</Text>
+`,`
+  const Styles = {h1: {fontSize: 32}, h2: {fontSize: 24}, 'flex-1': {flex: 1}}
+  const X = ({name}) => <Text className="h1 h2 flex-1" style={{fontSize: 42}}>name</Text>
+`]
 
-it('injects styles from classNames', () => {
-  const {code} = babel.transform(example, {plugins: [plugin], presets: ['@babel/preset-env', '@babel/preset-react']})
-  expect(code).toMatchSnapshot()
+examples.forEach(example => {
+  it('injects styles from classNames', () => {
+    const {code} = babel.transform(example, {plugins: [plugin], presets: ['@babel/preset-env', '@babel/preset-react']})
+    expect(code).toMatchSnapshot()
+  })
 })
